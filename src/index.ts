e4253b85aba18a73ef772f505af774dde00c9283
Beeno Tung <aabbcc1241@yahoo.com.hk>
2019-11-27 14:08:18 +0800

    added dark theme; persistent theme and interval to localStorage;

--- src/index.ts
+++ src/index.ts
@@ -3,37 +3,72 @@ import { format_time_duration } from '@beenotung/tslib/format';
 
 let unit = MINUTE;
 let unit_text = 'minute';
-let interval = 10 * unit;
+let interval = (+localStorage.getItem('interval') || 10) * unit;
 let interval_text = `${interval / unit} ${unit_text}s`;
+let theme = localStorage.getItem('theme') || 'light';
 let timer;
 
 export let dom = {
-  container: document.querySelector('#container') as HTMLDivElement,
-  interval: document.querySelector('#interval') as HTMLSpanElement,
-  change: document.querySelector('#change') as HTMLButtonElement,
-  start: document.querySelector('#start') as HTMLSpanElement,
-  end: document.querySelector('#end') as HTMLSpanElement,
-  progress: document.querySelector('#progress') as HTMLProgressElement,
-  left: document.querySelector('#left') as HTMLSpanElement,
+  container: document.getElementById('container') as HTMLDivElement,
+  interval: document.getElementById('interval') as HTMLSpanElement,
+  changeInterval: document.getElementById('change-interval') as HTMLButtonElement,
+  theme: document.getElementById('theme') as HTMLSpanElement,
+  changeTheme: document.getElementById('change-theme') as HTMLButtonElement,
+  start: document.getElementById('start') as HTMLSpanElement,
+  end: document.getElementById('end') as HTMLSpanElement,
+  progress: document.getElementById('progress') as HTMLProgressElement,
+  left: document.getElementById('left') as HTMLSpanElement,
+  style: document.createElement('style'),
 };
+document.head.appendChild(dom.style);
 
-// dom.container.style.display = 'initial';
+function renderTheme() {
+  dom.theme.innerText = theme;
+  if (theme === 'dark') {
+    dom.changeTheme.innerText='Change to light'
+    dom.style.innerHTML = `* {
+  background: black !important;
+  color: lightgreen !important;
+}`
+  } else {
+    dom.changeTheme.innerText='Change to dark'
+    dom.style.innerHTML=''
+  }
+}
 
-function initDom() {
+renderTheme();
+
+function renderInterval() {
   interval_text = `${interval / unit} ${unit_text}s`;
   dom.interval.innerText = interval_text;
   dom.progress.max = interval;
 }
 
-initDom();
-dom.change.onclick = e => {
+renderInterval();
+
+dom.changeInterval.onclick = e => {
   let answer = prompt('interval in unit of minute', (interval / MINUTE).toString());
   if (!answer) {
     return;
   }
+  localStorage.setItem('interval', answer);
   interval = +answer * MINUTE;
-  initDom();
+  renderInterval();
   init();
+};
+
+dom.changeTheme.onclick = e => {
+  switch (theme) {
+    case 'light':
+      theme = 'dark';
+      break;
+    case 'dark':
+      theme = 'light';
+      break;
+  }
+  localStorage.setItem('theme', theme);
+  dom.theme.innerText = theme;
+  renderTheme();
 };
 
 let start: number;
